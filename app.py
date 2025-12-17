87bec6d4c82ac0b606f78f617c09fbc3b12e66cd
EC2 Default User <ec2-user@ip-172-31-20-146.ec2.internal>
2025-12-17 06:13:08 +0000

    Added requirements.txt file and made /health,/hello

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on AWS CloudShell!"
+    return jsonify({
+        "message": "Flask app is running successfully",
+        "status": "OK"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "service": "flask-basic-app",
+        "health": "healthy"
+    })
+
+@app.route("/hello")
+def hello():
+    return jsonify({
+        "message": "Hello from Flask inside Docker"
+    })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
-
+    app.run(host="0.0.0.0", port=5000)
